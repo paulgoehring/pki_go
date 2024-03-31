61da4b60aecdac00d05d4551af15cd48324bab21
paulgoehring <56716458+paulgoehring@users.noreply.github.com>
2024-03-31 03:29:38 +0200

    Update start.sh

--- pki/root_server/start.sh
+++ pki/root_server/start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Command 1
-echo "Running command 1"
+marblerun manifest set manifest.json localhost:4433
+
 marblerun manifest verify manifest.json localhost:4433 --coordinator-cert marblerunCA.crt
 
-# Command 2
-echo "Running command 2"
-
 ./root_server
-# Command 3
-echo "Running command 3"
+
